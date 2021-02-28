2d1cd6ab14e66cdeed540b470ee6550c31a4132e
shi-li-feng <59203496+shi-li-feng@users.noreply.github.com>
2021-02-28 21:08:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,37 @@
-// version v0.0.2
+// version v0.0.1
 // create by ruicky
 // detail url: https://github.com/ruicky/jd_sign_bot
 
-const exec = require('child_process').execSync;
-const fs = require('fs');
-const rp = require('request-promise');
-const download = require('download');
+const exec = require('child_process').execSync
+const fs = require('fs')
+const rp = require('request-promise')
+const download = require('download')
 
 // 公共变量
-const KEY = process.env.JD_COOKIE;
-const serverJ = process.env.PUSH_KEY;
-const DualKey = process.env.JD_COOKIE_2;
-
+const KEY = process.env.JD_COOKIE
+const serverT = process.env.PUSH_TOKEN
 
 async function downFile () {
     // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
-    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
-    await download(url, './');
+    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
+    await download(url, './')
 }
 
-async function changeFile () {
+async function changeFiele () {
    let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
-   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
-   if (DualKey) {
-    content = content.replace(/var DualKey = ''/, `var DualKey = '${DualKey}'`);
-   }
+   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`)
    await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
 }
 
 async function sendNotify (text,desp) {
   const options ={
-    uri:  `https://sc.ftqq.com/${serverJ}.send`,
-    form: { text, desp },
-    json: true,
+    uri:  'https://leom.me:30088/wechat-work-pusher/card',
+    headers: { 'Cookie': `session=${serverT}`},
+    form: {
+        title: text,
+        description: desp,
+        url: 'https://home.m.jd.com'
+    },
     method: 'POST'
   }
   await rp.post(options).then(res=>{
@@ -51,25 +50,23 @@ async function start() {
   await downFile();
   console.log('下载代码完毕')
   // 替换变量
-  await changeFile();
+  await changeFiele();
   console.log('替换变量完毕')
   // 执行
   await exec("node JD_DailyBonus.js >> result.txt");
   console.log('执行完毕')
 
-  if (serverJ) {
+  if (serverT) {
     const path = "./result.txt";
     let content = "";
+    let trimContent = "";
     if (fs.existsSync(path)) {
       content = fs.readFileSync(path, "utf8");
+      trimContent = content.substring(content.indexOf('【签到概览】'),content.indexOf('京东商城-京豆:')-1)+'\n'+content.substring(content.indexOf('签到用时'),content.length)+'\n'+'点击前往京东查看账号详情~';
     }
-    let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
-    let res = t ? t[1].replace(/\n/,'') : '失败'
-    let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
-    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
-
-    
-    await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+    console.log(content)
+    await sendNotify("京东签到-" + new Date().toLocaleDateString(), trimContent);
+    console.log('发送结果完毕')
   }
 }
 
